6d0750fb197aca0396b4290312094a603f9451c9
VaibhavMojidra <vmojidra25@gmail.com>
2023-01-28 04:43:10 +0530

    If else and elseif

---
+++ README.md
@@ -0,0 +1,3 @@
+# Java Demo If Else And Elseif
+
+[![Vaibhav Mojidra - output.png](https://raw.githubusercontent.com/VaibhavMojidra/Java---Demo-If-Else-And-Elseif/master/Output/output.png "Vaibhav Mojidra")](https://vaibhavmojidra.github.io/site/)
---
+++ src/Demo.java
@@ -0,0 +1,28 @@
+
+public class Demo {
+	public static void main(String[] args) {
+		int a=5;
+		int b=6;
+		if(a==5) {
+			System.out.println("a is equal to 5");
+		}
+		
+		if(a>6) {
+			System.out.println("a is greater then 6");
+		}
+		else {
+			System.out.println("a is not greater then 6");
+		}
+		
+		if(b>6) {
+			System.out.println("b is greater then 6");
+		}
+		else if(b<6) {
+			System.out.println("b is small then 6");
+		}
+		else {
+			System.out.println("b is equal to 6");
+		}
+		
+	}
+}
